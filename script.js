dbb307848d19eb68f3867b252572afdd2376e688
Kiritchell <kiritchell@gmail.com>
2023-03-18 15:52:38 -0500

    --al

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
+let powerWritten = document.getElementById('power-el')
+let pastPower = document.getElementById('past-power')
 let power = 0
-let writtenPower = document.getElementById('power-el')
-let saveEl = document.getElementById('save-el')
+let roundNumberClicks = document.getElementById('save-btn')
+let count = 0
+let result = 0
 
 function increase(){
-    power += 1
-    writtenPower.innerHTML = power
+  power += 1
+  powerWritten.innerHTML = power
 }
 
 function decrease(){
-    power -= 1
-    writtenPower.innerHTML = power
+  power -= 1
+  powerWritten.innerHTML = power
 }
+
+
 
 function save(){
-    roundNumberClicks.onclick = function(){
-      count += 1;
-      console.log(count)
-      pastPower.innerHTML += 'Round ' + count + '  =  ' + power + '<br>'
-    }
+  roundNumberClicks.onclick = function(){
+    count += 1;
+    console.log(count)
+    pastPower.innerHTML += 'Round ' + count + '  =  ' + power + '<br>'
+
+  }
+
 }
-
-
-
