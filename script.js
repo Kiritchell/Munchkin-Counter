6b9e5a5edcd28618455615dc8d0ea935704cb0af
Kiritchell <kiritchell@gmail.com>
2023-03-26 20:55:02 -0500

    --all

--- script.js
+++ script.js
@@ -1,28 +1,37 @@
 let powerWritten = document.getElementById('power-el')
 let pastPower = document.getElementById('past-power')
-let power = 0
+let scoreFromLocalStorage = JSON.parse(localStorage.getItem('Power'))
+let power = scoreFromLocalStorage
 let roundNumberClicks = document.getElementById('save-btn')
 let count = 0
-let result = 0
+
+function load(){
+  if (scoreFromLocalStorage >=1){
+  powerWritten.innerHTML = scoreFromLocalStorage;}
+  else{powerWritten.innerHTML = 0}
+}
 
 function increase(){
   power += 1
   powerWritten.innerHTML = power
+  localStorage.setItem("Power", JSON.stringify(power))
 }
 
 function decrease(){
   power -= 1
   powerWritten.innerHTML = power
+  localStorage.setItem("Power", JSON.stringify(power))
 }
 
-
-
 function save(){
-  roundNumberClicks.onclick = function(){
+    roundNumberClicks.onclick = function(){
     count += 1;
-    console.log(count)
     pastPower.innerHTML += 'Round ' + count + '  =  ' + power + '<br>'
 
   }
+}
 
+function reset(){
+  localStorage.clear()
+  location.reload();
 }
